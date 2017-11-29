b278770e9db6e4a8ca22ce6384f9b17f146a414d
Arturo Burela <uno@arturoburela.com>
2017-11-29 15:44:53 -0600

    Api code

---
+++ common/models/analysis.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function (Analysis) {
+  /**
+   * Start images analysis
+   * @param {number} firstPhotoId First photo Id to start analysis
+   * @param {number} lastPhotoId Last photo Id to stop analysis
+   * @param {Function(Error, string, object)} callback
+   */
+
+  Analysis.start = function(firstPhotoId, lastPhotoId, callback) {
+    var mapPhoto, results;
+    mapPhoto = 'hola';
+    results = {};
+    // TODO
+    callback(null, mapPhoto, results);
+  };
+
+  Analysis.remoteMethod('start');
+
+};
