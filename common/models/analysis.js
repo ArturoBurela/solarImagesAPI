10a287d8fb2b38f510d9f3b8360f4a198426b6ff
Arturo Burela <uno@arturoburela.com>
2017-11-29 16:40:27 -0600

    Api code

--- common/models/analysis.js
+++ common/models/analysis.js
@@ -7,12 +7,24 @@ module.exports = function(Analysis) {
    * @param {number} lastPhotoId Last photo Id to stop analysis
    * @param {Function(Error, string, object)} callback
    */
+  const exec = require('child_process');
 
   Analysis.start = function(firstPhotoId, lastPhotoId, callback) {
     var mapPhoto, results;
     mapPhoto = 'hola';
     results = firstPhotoId.toString();
-    // TODO
+    // Get all images from blobstore and store them locally
+    // Use OpenDrone
+    var cmd = 'pwd';
+    exec(cmd, function(error, stdout, stderr) {
+      // command output is in stdout
+      console.log(error);
+      console.log(stdout);
+      console.log(stderr);
+    });
+    // Identify Objects in global map image
+    // Convert map to base 64
+    // return map picture and results
     var x = {mapPhoto: mapPhoto, results: results};
     callback(null, x);
   };
